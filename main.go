8a21f750cfa1324d8387997d97562c1694a582e4
Sebastian Widmer <sebastian.widmer@vshn.net>
2022-08-23 08:38:26 +0200

    Fix dummy domain from bootstrapping

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "c8138e14.my.domain",
+		LeaderElectionID:       "c8138e14.appuio.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
